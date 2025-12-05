cc90b1f8015d33a16b6f0eac3cbf4b95ba342047
sharpshrill <kshitij2301@gmail.com>
2025-12-05 10:44:29 +0530

    formatted teacher.cs

--- SmartAttendance/Models/Teacher.cs
+++ SmartAttendance/Models/Teacher.cs
@@ -1,26 +1,26 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
+﻿    using System.ComponentModel.DataAnnotations;
+    using System.Text.Json.Serialization;
 
-namespace SmartAttendance.Models
-{
-    public class Teacher
+    namespace SmartAttendance.Models
     {
-        [Key]
-        public string? TeacherId { get; set; }
+        public class Teacher
+        {
+            [Key]
+            public string? TeacherId { get; set; }
 
-        public string FullName { get; set; }
+            public string FullName { get; set; }
 
-        [EmailAddress]
-        public string Email { get; set; }
+            [EmailAddress]
+            public string Email { get; set; }
 
-        public string Password { get; set; }
-        public string UserId { get; set; } = default!;
-        public AppUser? User { get; set; }
+            public string Password { get; set; }
+            public string UserId { get; set; } = default!;
+            public AppUser? User { get; set; }
 
-        
-        public string? Department { get; set; }
+            
+            public string? Department { get; set; }
 
-        [JsonIgnore]
-        public List<Subject> Subjects { get; set; } = new List<Subject>();
+            [JsonIgnore]
+            public List<Subject> Subjects { get; set; } = new List<Subject>();
+        }
     }
-}
